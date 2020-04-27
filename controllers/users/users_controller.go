0b26983c3edd2439b6b34b374ae97c768ed27827
Mykola Shumyn <msh@adorsys.com.ua>
2020-04-27 21:07:01 +0300

    Created project structure and basic implementation of its components

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,30 @@
+package users
+
+import (
+	"bookstore_users-api/domain/users"
+	"bookstore_users-api/services"
+	"bookstore_users-api/utils/errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NawBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "not implemented yet")
+}
