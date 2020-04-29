5e0dc98de3be3836f69c74cc6c12e5199876a609
Mykola Shumyn <msh@adorsys.com.ua>
2020-04-29 20:27:45 +0300

    Implemented updating users in DB

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -8,19 +8,19 @@ import (
 )
 
 const (
-	errorNoRows     = "no rows in result set"
 	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
 	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = ?;"
+	queryUpdateUser = "UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE id = ?;"
 )
 
-func (user *User) Get(userId int64) *errors.RestErr {
+func (user *User) Get() *errors.RestErr {
 	stmt, err := users.Client.Prepare(queryGetUser)
 	if err != nil {
 		return errors.NewInternalServerError(err.Error())
 	}
 	defer stmt.Close()
 
-	result := stmt.QueryRow(userId)
+	result := stmt.QueryRow(user.Id)
 	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); getErr != nil {
 		mysql.ParseError(getErr)
 	}
@@ -48,5 +48,20 @@ func (user *User) Save() *errors.RestErr {
 	}
 
 	user.Id = userId
+	return nil
+}
+
+func (user *User) Update() *errors.RestErr {
+	stmt, err := users.Client.Prepare(queryUpdateUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.FirstName, user.LastName, user.Email, user.Id)
+	if err != nil {
+		return mysql.ParseError(err)
+	}
+
 	return nil
 }
